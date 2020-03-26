97c0a429cae0c2f9b7156e07d574183ce41c025f
Punitkumar Y Harsoor <39958570+punitkmryh@users.noreply.github.com>
2020-03-26 12:40:29 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Vue.js-Wishlist-app
+# Vue Todo
+
+A Simple Todo app built with [vue.js](https://vuejs.org/)
+
+![alt text](screenshot.png "Screenshot")
+
+## Project setup
+
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+
+```
+yarn run build
+```
+
+### Lints and fixes files
+
+```
+yarn run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p class="title">Vue.js Todo List</p>
+    <todo-list />
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  name: "app",
+  components: {
+    TodoList
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
+body {
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  color: #2c3e50;
+  font-family: Karla, sans-serif;
+  font-size: 16px;
+  margin: 0;
+}
+
+::-webkit-scrollbar {
+  height: 10px;
+  width: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+  background-clip: padding-box;
+  background-color: #b4c7d0;
+  border: 3px solid transparent;
+  border-radius: 5px;
+}
+
+::-webkit-scrollbar-track-piece {
+  background: 0 0;
+}
+
+#app {
+  max-width: 510px;
+  margin: 50px auto;
+  padding: 0 10px;
+}
+
+img {
+  display: block;
+  width: 80px;
+  height: 80px;
+  margin: 0 auto 15px auto;
+}
+
+p.title {
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
